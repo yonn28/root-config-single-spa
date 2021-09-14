dc7743db3411a0dc47b5c1b49bb4734734436fa3
yonny <ynova@bancodebogota.com.co>
2021-09-14 15:43:06 -0500

    my first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "org";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+  });
+};
